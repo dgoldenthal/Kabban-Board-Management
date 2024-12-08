57a9ebb539aad796af55178f827db6cfaf2c8a89
Dov Goldenthal <dgoldenthal@gmail.com>
2024-12-09 12:46:07 +1300

    update

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Sequelize } from 'sequelize';
-import { UserFactory } from './user.ts';
-import { TicketFactory } from './ticket.ts';
+import { UserFactory } from './user.js';
+import { TicketFactory } from './ticket.js';
 
 // For deployment - with SSL
 const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
