ff21e91840a4775a075bf017b06b5cecedcc0e5c
Dov Goldenthal <dgoldenthal@gmail.com>
2024-12-09 21:40:54 +1300

    fixed issues

--- client/src/components/TicketCard.tsx
+++ client/src/components/TicketCard.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { TicketData } from '../interfaces/TicketData';
 import { ApiMessage } from '../interfaces/ApiMessage';
 
